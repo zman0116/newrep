dce60cccb7562dd73362b8a74229805e0a91fbec
zman0116 <42784511+zman0116@users.noreply.github.com>
2018-12-02 03:30:45 -0600

    changes

---
+++ main.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$(".five").click(function(){
+	$(".five h2").text("any abbout and costelu");
+});
+
+$(".four").mouseup(function(){
+	$(".four h3").html("now you see me");
+});
+
+$(".three").mouseover(function(){
+$(".three h4").html("die hard 1and2");});
+
+$(".two").mouseenter(function(){
+$(".two h5").html("deadpool");});
+
+$(".one").mousemove(function(){
+$(".one h6").html("memories of murder");});
